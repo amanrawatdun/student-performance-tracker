b66876b9d56fc14451d6407cee0a83d8af8fb13e
Aman Rawat <amanwtsre@gmail.com>
2025-07-12 12:08:36 +0530

    Fix: safe token extraction in checkAuth middleware

--- backEnd/middlewares/checkAuth.js
+++ backEnd/middlewares/checkAuth.js
@@ -1,14 +1,22 @@
 const { getUser } = require("./auth");
 
- function checkAuth(req ,res ,next){
-   const authHeader = req.headers.authorization;
-   const token =authHeader.split(' ')[1];
-   
-   const user = getUser(token);
-   
-   req.user=user;
-   next();
+function checkAuth(req, res, next) {
+  const authHeader = req.headers.authorization;
+
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ message: "Unauthorized: No token provided" });
+  }
+
+  const token = authHeader.split(" ")[1];
+  const user = getUser(token);
+
+  if (!user) {
+    return res.status(403).json({ message: "Forbidden: Invalid token" });
+  }
+
+  req.user = user;
+  next();
 }
 
-
-module.exports=checkAuth;
+module.exports = checkAuth;
