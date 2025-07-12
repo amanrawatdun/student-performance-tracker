c480eadae537840009eab5113781a46924c13c9c
Aman Rawat <154727782+amanrawatdun@users.noreply.github.com>
2025-07-12 12:15:21 +0530

    Update api.js

--- frontEnd/src/services/api.js
+++ frontEnd/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const API=axios.create({
-    baseURL:'http://localhost:8000'
+    baseURL:'https://student-performance-tracker-backend-cdc0.onrender.com'
 });
 
 // Add token to requests automatically
